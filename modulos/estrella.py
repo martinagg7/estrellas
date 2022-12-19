6f763bc5fe3bb1e012644d1c488ae50420e19ee2
martinagg7 <114728426+martinagg7@users.noreply.github.com>
2022-12-19 22:38:04 +0100

    Update estrella.py

--- modulos/estrella.py
+++ modulos/estrella.py
@@ -2,6 +2,7 @@ import turtle
 
 estrella = turtle.Pen()
 
+#ajustes estrella 
 estrella.fillcolor('yellow')
 estrella.begin_fill()
 
@@ -10,23 +11,5 @@ n=int(input('¿Cuántas puntas quiere que tenga la estrella?'))
 for i in range(n):
     estrella.forward(100)
     estrella.right(180-180/n)
-
-import turtle 
-
-estrella=turtle.Pen()
-estrella.shape('star')
-
-estrella.fillcolor('yellow')
-estrella.color('orange')
-
-estrella.begin_fill()
-
-n=int(input('¿Cuántas puntas quiere que tenga la estrella?'))
-
-for i in range(n):
-    estrella.foward(100)
-    estrella.right(180-180/n)
-
-
 estrella.end_fill()
 
